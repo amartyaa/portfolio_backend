bb8a463af9ad6c1a618359e40911b1a5ba3d022e
Amartya Anshuman <amartya.anshuman@vodafone.com>
2022-12-13 13:55:22 +0000

    added db handler

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/aws/aws-lambda-go v1.36.0
+	github.com/aws/aws-sdk-go v1.44.158 // indirect
 	github.com/valyala/fastjson v1.6.3
 )
--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/valyala/fastjson"
 )
 
@@ -30,8 +35,54 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	return ApiResponse, nil
 }
 
-func dbHandler() {
-	
+func dbHandler() *dynamodb.PutItemOutput {
+	// code to write message to dynamodb
+	svc := dynamodb.New(session.New())
+	input := &dynamodb.PutItemInput{
+		Item: map[string]*dynamodb.AttributeValue{
+			"Name": {
+				S: aws.String("Somewhat Famous"),
+			},
+			"Email": {
+				S: aws.String("No One You Know"),
+			},
+			"Message": {
+				S: aws.String("Call Me Today"),
+			},
+		},
+		ReturnConsumedCapacity: aws.String("TOTAL"),
+		TableName:              aws.String(os.Getenv("TABLE_NAME")),
+	}
+
+	result, err := svc.PutItem(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case dynamodb.ErrCodeConditionalCheckFailedException:
+				fmt.Println(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
+			case dynamodb.ErrCodeProvisionedThroughputExceededException:
+				fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+			case dynamodb.ErrCodeResourceNotFoundException:
+				fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
+			case dynamodb.ErrCodeItemCollectionSizeLimitExceededException:
+				fmt.Println(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
+			case dynamodb.ErrCodeTransactionConflictException:
+				fmt.Println(dynamodb.ErrCodeTransactionConflictException, aerr.Error())
+			case dynamodb.ErrCodeRequestLimitExceeded:
+				fmt.Println(dynamodb.ErrCodeRequestLimitExceeded, aerr.Error())
+			case dynamodb.ErrCodeInternalServerError:
+				fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+
+	}
+	return result
 }
 
 func main() {
